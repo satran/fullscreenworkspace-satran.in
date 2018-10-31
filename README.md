447a11fae659ca5d9bce86fb0f0183d2e241fe29
Satyajit Ranjeev <s@ranjeev.in>
2018-11-01 00:44:53 +0100

    Make it work only with fullscreen

---
+++ README.md
@@ -0,0 +1,11 @@
+# Gnome Shell Extension Fullscreen to Workspace
+
+I got inspired by a feature by Elementary OS (which comes from macOS). It moves a fullscreen application to a separate workspace. This extension does just that. A lot of the code ideas come from https://github.com/rliang/gnome-shell-extension-maximize-to-workspace.
+
+# Installation from source
+git clone https://github.com/satran/fullscreenworkspace@satran.in ~/.local/share/gnome-shell/extensions/fullscreenworkspace@satran.in
+gnome-shell-extension-tool -e fullscreenworkspace@satran.in
+
+# Caveats
+- I have only tested it with GNOME 3.30.1
+
--- extension.js
+++ extension.js
@@ -1,23 +1,15 @@
 const Meta = imports.gi.Meta;
 
-function init() {
-    
-}
-const _handles = [];
-
+var _handles = [];
 var _previousWorkspace = {};
 
 function maximize(act) {
     const win = act.meta_window;
-    //if (win.window_type !== Meta.WindowType.NORMAL)
-    	//return;
-    if (win.get_maximized() !== Meta.MaximizeFlags.BOTH)
-	return;
-
+    if (win.window_type !== Meta.WindowType.NORMAL)
+    	return;
     // If the current workspace doesn't have any other windows make it maximized here.
     if (global.workspace_manager.get_active_workspace().list_windows().length == 1)
 	return;
-
     _previousWorkspace[win.toString()] = global.workspace_manager.get_active_workspace_index();
     let lastworkspace = global.workspace_manager.n_workspaces;
     if (lastworkspace<1)
@@ -28,9 +20,8 @@ function maximize(act) {
 
 function unmaximize(act){
     const win = act.meta_window;
-    // Meta.MaximizeFlags doesn't have anything for it not being set, 0 means it is not maximized
-    if (win.get_maximized() !== 0)
-	return;
+    if (win.window_type !== Meta.WindowType.NORMAL)
+    	return;
     let previous = _previousWorkspace[win.toString()];
     if (previous == null || previous == undefined)
 	return;
@@ -39,11 +30,9 @@ function unmaximize(act){
 }
 
 function enable() {
-    //global.get_window_actors().forEach(check);
-    _handles.push(global.window_manager.connect('map', (_, act) => maximize(act)));
     _handles.push(global.window_manager.connect('size-change', (_, act, change) => {
-	if (change === Meta.SizeChange.MAXIMIZE) maximize(act);
-	if (change === Meta.SizeChange.UNMAXIMIZE) unmaximize(act);
+	if (change === Meta.SizeChange.FULLSCREEN) maximize(act);
+	if (change === Meta.SizeChange.UNFULLSCREEN) unmaximize(act);
     }));
 }
 
