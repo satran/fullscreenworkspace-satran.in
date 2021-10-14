1997da46e84c59e32f1f7182000f089ecdae7b87
Egor Vorontsov <sdoregor@sdore.me>
2021-10-14 15:27:21 +0300

    Refactored code, changed some logic. Fixes #1 New workspace is placed next to active instead of last. Windows fullscreened on non-active workspaces are ignored. Closing the window restores its original workspace.

--- extension.js
+++ extension.js
@@ -4,38 +4,60 @@ var _handles = [];
 var _previousWorkspace = {};
 
 function maximize(act) {
-    const win = act.meta_window;
-    if (win.window_type !== Meta.WindowType.NORMAL)
-    	return;
-    // If the current workspace doesn't have any other windows make it maximized here.
-    if (global.workspace_manager.get_active_workspace().list_windows().length == 1)
-	return;
-    _previousWorkspace[win.toString()] = global.workspace_manager.get_active_workspace_index();
-    let lastworkspace = global.workspace_manager.n_workspaces;
-    if (lastworkspace<1)
-	lastworkspace=1;
-    win.change_workspace_by_index(lastworkspace,1);
-    global.workspace_manager.get_workspace_by_index(lastworkspace).activate(global.get_current_time());
+	const win = act.meta_window;
+	if (win.window_type !== Meta.WindowType.NORMAL) return;
+
+	let active = global.workspace_manager.get_active_workspace();
+	if (win.get_workspace() != active) return;
+
+	// If the current workspace doesn't have any other windows make it maximized here.
+	if (active.list_windows().length == 1) return;
+
+	_previousWorkspace[win] = active;
+
+	let last = Math.max(1, global.workspace_manager.get_n_workspaces()-1);
+	let ws = global.workspace_manager.get_workspace_by_index(last);
+	win.change_workspace(ws);
+	global.workspace_manager.reorder_workspace(ws, active.index()+1);
+	ws.activate(global.get_current_time());
 }
 
-function unmaximize(act){
-    const win = act.meta_window;
-    if (win.window_type !== Meta.WindowType.NORMAL)
-    	return;
-    let previous = _previousWorkspace[win.toString()];
-    if (previous == null || previous == undefined)
-	return;
-    win.change_workspace_by_index(previous, 1);
-    global.workspace_manager.get_workspace_by_index(previous).activate(global.get_current_time());
+function unmaximize(act) {
+	const win = act.meta_window;
+	if (win.window_type !== Meta.WindowType.NORMAL) return;
+
+	let previous = _previousWorkspace[win];
+	if (!previous) return;
+
+	win.change_workspace(previous);
+	previous.activate(global.get_current_time());
+
+	delete _previousWorkspace[win];
+}
+
+function close(act) {
+	const win = act.meta_window;
+	if (win.window_type !== Meta.WindowType.NORMAL) return;
+
+	let previous = _previousWorkspace[win];
+	if (!previous) return;
+
+	previous.activate(global.get_current_time());
+
+	delete _previousWorkspace[win];
 }
 
 function enable() {
-    _handles.push(global.window_manager.connect('size-change', (_, act, change) => {
-	if (change === Meta.SizeChange.FULLSCREEN) maximize(act);
-	if (change === Meta.SizeChange.UNFULLSCREEN) unmaximize(act);
-    }));
+	let wm = global.window_manager;
+
+	_handles.push(wm.connect('size-change', (_, act, change) => {
+		if (change === Meta.SizeChange.FULLSCREEN) maximize(act);
+		if (change === Meta.SizeChange.UNFULLSCREEN) unmaximize(act);
+	}));
+
+	_handles.push(wm.connect('destroy', (_, act) => close(act)));
 }
 
 function disable() {
-    _handles.splice(0).forEach(h => global.window_manager.disconnect(h));
+	_handles.forEach(h => global.window_manager.disconnect(h));
 }
